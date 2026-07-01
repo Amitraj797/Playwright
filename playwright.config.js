d7144767a6e375a23f62a07a17817989602fa479
Amitraj797 <amitraj797@gmail.com>
2026-07-01 22:43:43 +0530

    cofig file fixes

--- playwright.config.js
+++ playwright.config.js
@@ -1,25 +1,22 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
-import { trace } from 'node:console';
-
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
- const config=({
+const config = {
   testDir: './tests',
   timeout: 40000,
-  screenshot: "only-on-failure",
-  trace:'retain-on-failure',
-  expect:{
-    timeout:40000
+  screenshot: 'only-on-failure',
+  trace: 'retain-on-failure',
+  expect: {
+    timeout: 40000,
   },
   reporter: 'html',
-  use:{
-    browserName:'chromium',
+  use: {
+    browserName: 'chromium',
     headless: false,
   },
-});
+};
 
-module.exports=config
+module.exports = config;
 
